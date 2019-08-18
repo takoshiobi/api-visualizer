4c5c27d032b7a2aafe6ddb1c6fc79babab9dc6d4
akinariobi <yuliyanasher@gmail.com>
2019-08-19 01:55:10 +0300

    add location input

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import LocationInput from './components/LocationInput';
+
+function App() {
+  return (
+    <div className="App_container">
+      <LocationInput />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LocationInput.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './LocationInput.css';
+
+class LocationInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+    
+      handleChange(event) {
+        this.setState({value: event.target.value});
+      }
+    
+      handleSubmit(event) {
+        alert('Sent location: ' + this.state.value);
+        event.preventDefault();
+      }
+    
+      render() {
+        return (
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Location:
+              <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Go" />
+          </form>
+        );
+      }
+}
+
+export default LocationInput;
